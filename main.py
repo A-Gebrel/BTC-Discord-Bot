b0df2cc6a49ebe06b86dbf9c894e7ac24d3a899e
SirAnxious <SirAnxiousALot@mail.com>
2023-06-12 01:12:06 +0300

    Add unconfirmed transaction count

--- main.py
+++ main.py
@@ -7,18 +7,21 @@ def get_config():
         config = json.load(file_object)
     return config
 
-config = get_config()
-
-crypto_list = {"BTC", "ETH", "BCH", "LTC", "XRP", "SOL", "XMR", "DOGE"}
-# Ask for Token on start
-# Another way of using this would be a config file or env variables
-DISCORD_TOKEN = config['discord_token']
-API_KEY = config['api_key']
-
 # Check if config.json exists in the directory
 if(os.path.exists('config.json') == False):
     print("[ERROR] Config.json not found.")
     exit()
+
+# Get our Config (APIs/Tokens)
+config = get_config()
+
+# Currently supported Crypto list
+crypto_list = {"BTC", "ETH", "BCH", "LTC", "XRP", "SOL", "XMR", "DOGE"}
+
+# Ask for Token on start
+# Another way of using this would be a config file or env variables
+DISCORD_TOKEN = config['discord_token']
+API_KEY = config['api_key']
 
 # Let's confirm Discord Token is -probably- valid from default config.json
 if(DISCORD_TOKEN == "discord_bot_token_here" or DISCORD_TOKEN == None or DISCORD_TOKEN == ""):
@@ -73,17 +76,20 @@ async def check(interaction, txid:str):
 
 @tree.command(name = "fees", description = "Used to get optimal BTC fees")
 async def fees(interaction):
-    res = requests.get(f'https://mempool.space/api/v1/fees/recommended')
-    if(res.status_code != 200):
+    res = requests.get('https://mempool.space/api/v1/fees/recommended')
+    res2 = requests.get("https://mempool.space/api/mempool")
+    if(res.status_code != 200 or res2.status_code != 200):
         await interaction.response.send_message(f"Is mempool down? :'(")
     else:
         resp = res.json()
+        resp2 = res2.json()
         embed=discord.Embed(title="Optimal Fees", url=f"https://mempool.space/", description="Below you can see all the suggested fees.\n Remember that mining blocks might take longer if the network is congested.", color=0x04ff00)
         embed.add_field(name=f"Within 10 mins (next block)", value=f"{resp['fastestFee']} sat/vB", inline=False)
         embed.add_field(name=f"Within 30 mins", value=f"{resp['halfHourFee']} sat/vB", inline=False)
         embed.add_field(name=f"Within 60 mins", value=f"{resp['hourFee']} sat/vB", inline=False)
         embed.add_field(name=f"Don't hold your breath ", value=f"{resp['economyFee']} sat/vB", inline=False)
         embed.add_field(name=f"Won't be purged: ", value=f"{resp['minimumFee']} sat/vB", inline=False)
+        embed.add_field(name=f"Currently Unconfirmed Transactions:", value=f"{resp2['count']:,} TXs", inline=False)
         embed.add_field(name=f"Fetched at", value=f"<t:{int(time.time())}:R>")
         embed.set_footer(text="Made with ❤ by banonkiel#0001")
         await interaction.response.send_message(embed=embed)
